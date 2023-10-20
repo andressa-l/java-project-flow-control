fb74aad5054fa5c460a574af53ee8616ccf2b0d9
Andressa Lima <gregorio.andressa.lima@gmail.com>
2023-10-20 17:04:55 -0300

    first commit

---
+++ src/Contador.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Contador {
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        for (int i = parametroUm; i <= parametroDois; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
